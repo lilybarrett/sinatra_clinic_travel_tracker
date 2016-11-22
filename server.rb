b50c6b55513b69d7324e0cbb698970cfe9dd4711
lilybarrett <lily@barrett.us>
2016-11-22 10:35:23 -0500

    Finish working branch

--- server.rb
+++ server.rb
@@ -1,2 +1,21 @@
 require 'sinatra'
 require 'pry'
+
+get "/" do
+  redirect "/traveled_to_list"
+end
+
+get '/traveled_to_list' do
+  @traveled_to_list = File.readlines('traveled_to.txt')
+
+  erb :index
+end
+
+post "/traveled_to_list" do
+  latest_trip = params[:traveled_to]
+  File.open('traveled_to.txt', 'a') do |file|
+    file.puts(latest_trip)
+  end
+
+  redirect "/traveled_to_list"
+end
